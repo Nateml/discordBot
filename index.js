8011160a3776964e6e3a5644ff6c35c3a5d64dbf
Nateml <nateml.mac@gmail.com>
2020-10-10 11:00:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NzY0Mzg5MTI0NzAxNDIxNTY5.X4Fi2A.mmu8OnREvWPTS-J0W9oSG8wIDGc');
